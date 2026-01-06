028ed2babe23e3ee3f4c9dadc8aefa39ec584860
ahmedfarghaly <ahmedfarghaly505@gmail.com>
2026-01-06 18:26:34 +0200

    add alignment

--- src/TableCommandExtension.ts
+++ src/TableCommandExtension.ts
@@ -1,4 +1,4 @@
-import { Extension } from "@tiptap/core";
+import { Extension, findParentNode } from '@tiptap/core';
 import duplicateColumn from "./commands/duplicateColumn";
 import duplicateRow from "./commands/duplicateRow";
 
@@ -19,6 +19,49 @@ export const TableCommandExtension = Extension.create({
           duplicateRow(state, dispatch, withContent);
           return true;
         },
+        setTableAlign:
+            (align: 'left' | 'center' | 'right' | 'start' | 'end' | 'justify') =>
+                ({ state, dispatch }) => {
+                    const table = findParentNode(n => n.type.name === 'table')(state.selection);
+                    if (!table) return false;
+
+                    const tr = state.tr;
+                    let changed = false;
+
+                    table.node.descendants((node, pos) => {
+                        if (node.type.name === 'tableRow') {
+                            changed = true;
+                            tr.setNodeMarkup(table.pos + 1 + pos, undefined, {
+                                ...node.attrs,
+                                textAlign: align,
+                            });
+                        }
+                    });
+
+                    if (changed && dispatch) dispatch(tr);
+                    return changed;
+                },
+
+        unsetTableAlign:
+            () =>
+                ({ state, dispatch }) => {
+                    const table = findParentNode(n => n.type.name === 'table')(state.selection);
+                    if (!table) return false;
+
+                    const tr = state.tr;
+                    let changed = false;
+
+                    table.node.descendants((node, pos) => {
+                        if (node.type.name === 'tableRow' && node.attrs?.textAlign) {
+                            changed = true;
+                            const { textAlign, ...rest } = node.attrs;
+                            tr.setNodeMarkup(table.pos + 1 + pos, undefined, rest);
+                        }
+                    });
+
+                    if (changed && dispatch) dispatch(tr);
+                    return changed;
+                },
     };
   },
 });
--- src/pagination/TableRowPlus.ts
+++ src/pagination/TableRowPlus.ts
@@ -1,18 +1,55 @@
 import TableRow from "@tiptap/extension-table-row";
 
 export const TableRowPlus = TableRow.extend({
-    addNodeView() {
-        return ({}) => {
-          const dom = document.createElement('tr');
-          dom.style.display = 'grid';
-          dom.style.gridTemplateColumns = `var(--cell-percentage)`;
-          dom.style.position = "relative";
-          return {
-            dom,
-            contentDOM :  dom,
-          };
+    addAttributes() {
+        return {
+            ...this.parent?.(),
+            textAlign: {
+                default: null,
+                parseHTML: el =>
+                    el.style.textAlign ||
+                    el.getAttribute('data-align') ||
+                    el.getAttribute('align') ||
+                    null,
+                renderHTML: attrs => {
+                    if (!attrs.textAlign) return {};
+                    return { style: `text-align:${attrs.textAlign}`, 'data-align': attrs.textAlign };
+                },
+            },
         };
-      },
-})
+    },
 
-export default TableRowPlus
+    addNodeView() {
+        return ({ node }) => {
+            const dom = document.createElement('tr');
+            dom.style.display = 'grid';
+            dom.style.gridTemplateColumns = `var(--cell-percentage)`;
+            dom.style.position = "relative";
+
+            const applyAlign = (n: typeof node) => {
+                const align = n.attrs.textAlign;
+                if (align) {
+                    dom.style.textAlign = align;
+                    dom.dataset.align = align;
+                } else {
+                    dom.style.textAlign = '';
+                    delete dom.dataset.align;
+                }
+            };
+
+            applyAlign(node);
+
+            return {
+                dom,
+                contentDOM: dom,
+                update(updatedNode) {
+                    if (updatedNode.type.name !== 'tableRow') return false;
+                    applyAlign(updatedNode);
+                    return true;
+                },
+            };
+        };
+    },
+});
+
+export default TableRowPlus;
